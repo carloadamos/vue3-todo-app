214a151cc5d68711e60a8bd74a29b41b3a057be3
carloadamos <johncarloadamos@gmail.com>
2023-01-11 19:44:53 +0800

    todo list

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>Container</div>
+  <div class="container">
+    <input v-model="task" type="text" />
+    <button @click="createTask">Create</button>
+    <TodoList :tasks="tasks" />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive, ref } from "@vue/reactivity";
+import TodoList from "../components/TodoList.vue";
+
+const task = ref("");
+let tasks = reactive([
+  { id: 1, name: "cooking" },
+  { id: 2, name: "eating" },
+  { id: 3, name: "dancing" },
+]);
+
+const createTask = () => {
+  const newId = tasks.length;
+  tasks.push({ id: newId, name: task.value });
+};
+</script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Todolist</div>
+  <ul>
+    <li v-for="task in tasks" :key="task.id">{{ task.name }}</li>
+  </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+  tasks: Array,
+});
+</script>
+
+<style lang="scss" scoped></style>
